e121899349683f5364a0035e8e1c3a472ea09c72
Tripamungkas Kartika Adi <tripamungkas.adi@gmail.com>
2020-06-22 11:14:03 +0700

    add program to plot measured data

---
+++ PID_Plot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 18:47:21 2020
+
+@author: gnumi34
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from scipy.fftpack import fft
+from scipy.signal import butter, lfilter, freqz
+
+file_name = input("Enter file name : ")
+
+data = pd.read_csv(file_name)
+
+# Number of samplepoints
+N = len(data)
+T = 35
+t_plt = np.linspace(0, T, N, endpoint=False)
+
+fig, ax = plt.subplots(2,1)
+ax[0].set_title("Flow Rate Measurement")
+ax[0].plot(t_plt, data['Flow Rate (uL/min)'], 'r-', label='Measured Flow')
+ax[0].set_xlabel('Time [sec]')
+ax[0].set_ylabel('Flow Rate [uL/min]')
+ax[0].grid(True)
+
+ax[1].set_title("Control Measurement")
+ax[1].plot(t_plt, data['U_Now'], 'g-', label='Control Signal')
+ax[1].plot(t_plt, data['Frequency'], 'b-', label='STEP Frequency')
+ax[1].set_xlabel('Time [sec]')
+ax[1].set_ylabel('Frequency [Hz]')
+ax[1].grid(True)
+
+fig.tight_layout()
+
+# plt.subplots_adjust(hspace=0.35)
+plt.show()
