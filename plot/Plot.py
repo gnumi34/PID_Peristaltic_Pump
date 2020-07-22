9cd5084fd9b1635581680b6ef42754e5e4627f6e
gnumi34 <tripamungkas.adi@gmail.com>
2020-07-23 01:03:23 +0700

    add arguments and legend

--- plot/Plot.py
+++ plot/Plot.py
@@ -6,12 +6,16 @@ Created on Wed Feb 26 18:47:21 2020
 @author: gnumi34
 """
 
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-file_name = input("Enter file name : ")
+try:
+    file_name = sys.argv[1]
+except IndexError:
+    raise SystemExit(f"usage: {sys.argv[0]} csv_file")
 
 data = pd.read_csv(file_name)
 
@@ -27,14 +31,15 @@ ax[0].plot(t_plt, data['Flow Rate (uL/min)'], 'r-', label='Measured Flow')
 ax[0].set_xlabel('Time [sec]')
 ax[0].set_ylabel('Flow Rate [uL/min]')
 ax[0].grid(True)
+ax[0].legend()
 
 ax[1].set_title("Control Measurement")
 ax[1].plot(t_plt, data['U_Now'], 'g-', label='Control Signal')
-ax[1].plot(t_plt, data['Frequency'], 'b-', label='STEP Frequency')
+ax[1].plot(t_plt, data['Frequency'], 'b-', label='Motor Step Frequency')
 ax[1].set_xlabel('Time [sec]')
 ax[1].set_ylabel('Frequency [Hz]')
 ax[1].grid(True)
-
+ax[1].legend()
 fig.tight_layout()
 
 # plt.subplots_adjust(hspace=0.35)
