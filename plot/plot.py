9cf7e916fae0052643999ffdfc707c5e9dfd3a3f
gnumi34 <tripamungkas.adi@gmail.com>
2020-08-12 20:58:54 +0700

    Code refactor and add totalizer

--- plot/plot.py
+++ plot/plot.py
@@ -25,6 +25,10 @@ if not file_name[:3].isnumeric():
 else:
     flow_rate_sp = float(file_name[:3])
 
+print(f"File name: {file_name}")
+print(f"Set-point = {flow_rate_sp}")
+print("Processing data.....\n")
+
 # Number of samplepoints
 N = len(data)
 Time = data['Time (s)']
@@ -32,13 +36,17 @@ T = Time[N-1]
 t_plt = np.linspace(0, T, N, endpoint=False)
 
 # Calculate Average Flow Rate
+print("Calculating average flow rate based on realtime flow rate measurement..")
 sum = 0
 for i in range(N):
     sum += data['Flow Rate (uL/min)'][i]
 
-print(f'Average flow rate: {sum/N}')
+average_fr = sum/N
+
+print(f'Average flow rate: {average_fr} uL/m\n')
 
 # Calculate average deviation based on realtime flow rate
+print("Calculating average flow rate deviation based on realtime flow rate..")
 avg_min = 0
 avg_max = 0
 count_min = 0
@@ -55,63 +63,96 @@ for i in range(N):
 avg_max = avg_max / count_max
 avg_min = avg_min / count_min
 
-print(f'Average Max. Deviation: {avg_max} / {avg_max - flow_rate_sp}')
-print(f'Average Min. Deviation: {avg_min} / {flow_rate_sp - avg_min}')
-print(f'Average Deviation: +-{((avg_max - flow_rate_sp) + (flow_rate_sp - avg_min)) / 2}')
+print(f'Average Max. Deviation: {avg_max} uL/m / {avg_max - flow_rate_sp} uL/m')
+print(f'Average Min. Deviation: {avg_min} uL/m / {flow_rate_sp - avg_min} uL/m')
+print(f'Average Deviation: +-{((avg_max - flow_rate_sp) + (flow_rate_sp - avg_min)) / 2} uL/m\n')
 
 # Calculate average deviation based on moving average flow rate
+print("Calculating average flow rate deviation based on moving average flow rate..")
 avg_min = 0
 avg_max = 0
 count_min = 0
 count_max = 0
 
-for i in range(N):
-    if data['Moving Average Flow Rate (uL/min)'][i] >= flow_rate_sp:
-        avg_max += data['Moving Average Flow Rate (uL/min)'][i]
-        count_max += 1
-    else:
-        avg_min += data['Moving Average Flow Rate (uL/min)'][i]
-        count_min += 1
+if 'Moving Average Flow Rate (uL/min)' in data:
+    for i in range(N):
+        if data['Moving Average Flow Rate (uL/min)'][i] >= flow_rate_sp:
+            avg_max += data['Moving Average Flow Rate (uL/min)'][i]
+            count_max += 1
+        else:
+            avg_min += data['Moving Average Flow Rate (uL/min)'][i]
+            count_min += 1
 
-avg_max = avg_max / count_max
-avg_min = avg_min / count_min
+    avg_max = avg_max / count_max
+    avg_min = avg_min / count_min
 
-print(f'Average Max. Deviation (from Moving Average): {avg_max} / {avg_max - flow_rate_sp}')
-print(f'Average Min. Deviation (from Moving Average): {avg_min} / {flow_rate_sp - avg_min}')
-print(f'Average Deviation (from Moving Average): +-{((avg_max - flow_rate_sp) + (flow_rate_sp - avg_min)) / 2}')
+    print(f'Average Max. Deviation (from Moving Average): {avg_max} / {avg_max - flow_rate_sp}')
+    print(f'Average Min. Deviation (from Moving Average): {avg_min} / {flow_rate_sp - avg_min}')
+    print(f'Average Deviation (from Moving Average): +-{((avg_max - flow_rate_sp) + (flow_rate_sp - avg_min)) / 2}\n')
+else:
+    print("Moving average flow rate data is not found. Skipping..\n")
 
 # Calculate rise time based on average flow rate
+print("Calculating rise time of system (10% of set-point to 90% of set-point)..")
 max_time = 0
 min_time = 0
 
-for i in range(N):
-    if (data['Time (s)'][i] >= 0.2) and (data['Average Flow Rate (uL/min)'][i] >= (0.9 * flow_rate_sp)):
-        max_time = data['Time (s)'][i]
-        print(f'max_time = {max_time}')
-        break
+if 'Average Flow Rate (uL/min)' in data:
+    for i in range(N):
+        if (data['Time (s)'][i] >= 0.2) and (data['Average Flow Rate (uL/min)'][i] >= (0.9 * flow_rate_sp)):
+            max_time = data['Time (s)'][i]
+            print(f'max_time = {max_time} s')
+            break
 
-for i in range(N):
-    if (data['Time (s)'][i] >= 0.2) and (data['Average Flow Rate (uL/min)'][i] >= (0.1 * flow_rate_sp)):
-        min_time = data['Time (s)'][i]
-        print(f'max_time = {min_time}')
-        break
+    for i in range(N):
+        if (data['Time (s)'][i] >= 0.2) and (data['Average Flow Rate (uL/min)'][i] >= (0.1 * flow_rate_sp)):
+            min_time = data['Time (s)'][i]
+            print(f'min_time = {min_time} s')
+            break
 
-print(f'Rise time = {max_time - min_time}')
+    print(f'Rise time = {max_time - min_time} s\n')
+else:
+    print("Realtime Average Flow Rate data is not found. Skipping..\n")
+
+# Calculate dispensed volume using real-time flow rate measurement
+print("Calculating dispensed volume using realtime flow rate measurement..")
+totalizer = 0.0
+last_flow = 0.0
+last_time = 0.0
+for i in range(N):
+    totalizer += (last_flow + data['Flow Rate (uL/min)'][i]) / 2.0 * (data['Time (s)'][i] - last_time) / 60.0
+    last_flow = data['Flow Rate (uL/min)'][i]
+    last_time = data['Time (s)'][i]
+
+print(f"Dispensed volume in real-time = {totalizer:.5f} uL\n")
+
+# Calculate dispensed volume using average flow rate
+print("Calculating dispensed volume using realtime average flow rate measurement..")
+totalizer_avg = 0.0
+last_time = 0.0
+if 'Average Flow Rate (uL/min)' in data:
+    for i in range(N):
+        totalizer_avg += data['Average Flow Rate (uL/min)'][i] / 60.0 * (data['Time (s)'][i] - last_time)
+        last_time = data['Time (s)'][i]
+else:
+    print("Realtime average flow rate data is not found. Calculating using calculated average flow rate..")
+    for i in range(N):
+        totalizer_avg += average_fr / 60.0 * (data['Time (s)'][i] - last_time)
+        last_time = data['Time (s)'][i]
+
+print(f"Dispensed volume in average = {totalizer_avg:.5f} uL\n")
 
 # Plot data
+print("Plotting data..")
 fig, ax = plt.subplots(2,1)
 ax[0].set_title("Flow Rate Measurement")
 ax[0].plot(t_plt, data['Flow Rate (uL/min)'], 'r-', label='Measured Flow')
 
-try:
+if 'Moving Average Flow Rate (uL/min)' in data:
     ax[0].plot(t_plt, data['Moving Average Flow Rate (uL/min)'], 'b-', label='Moving Average Flow')
-except KeyError:
-    pass
 
-try:
+if 'Average Flow Rate (uL/min)' in data:
     ax[0].plot(t_plt, data['Average Flow Rate (uL/min)'], 'g-', label='Instantenous Average Flow Rate')
-except KeyError:
-    pass
 
 ax[0].set_xlabel('Time [sec]')
 ax[0].set_ylabel('Flow Rate [uL/min]')
@@ -128,3 +169,4 @@ ax[1].legend()
 fig.tight_layout()
 
 plt.show()
+print("Data processing is completed. Exiting..")
